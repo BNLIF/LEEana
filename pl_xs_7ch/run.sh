30855941bc41e34573ee680d2afeb96e6dc65ebc
Nitish Nayak <felarof.nayak@gmail.com>
2023-01-09 10:39:28 -0500

    forgot to remove this

--- pl_xs_7ch/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./convert_histo.pl 1
-#./run_det_sys.pl  #seg faults for some reason
-./run_xf_sys.pl
-xs_cov_matrix -r17
-merge_hist -r0 -l0 -e2 | tee ../mc_stat/xs_tot.log
-merge_hist_xs -r0 -l0
